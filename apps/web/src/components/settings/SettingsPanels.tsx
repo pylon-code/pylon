ff649bc4b2ed01204220b4445f4fdf493a8753a9
Chris Deeming <chris@xenforo.com>
2026-08-08 04:31:47 +0100

    fix(web): inherit terminal size in simple typography (#5628)
    (cherry picked from commit daf8ee0b2f684cda82e8721abe1138b219b1ef12)

--- apps/web/src/components/settings/SettingsPanels.tsx
+++ apps/web/src/components/settings/SettingsPanels.tsx
@@ -97,6 +97,7 @@ import {
   isMonospaceFamily,
   resolveDefaultFamilyLabel,
   resolveTerminalFontPreference,
+  resolveTerminalFontSizePreference,
   TYPOGRAPHY_ADVANCED_STORAGE_KEY,
 } from "../../appearanceFonts";
 import { CodeFontPreview, PromptFontPreview, TerminalFontPreview } from "./SettingsFontPreviews";
@@ -1283,7 +1284,11 @@ function SimpleFontRows() {
                 code: settings.fontFamilyCode,
                 terminal: settings.fontFamilyTerminal,
               })}
-              size={settings.fontSizeTerminal}
+              size={resolveTerminalFontSizePreference({
+                advanced: false,
+                code: settings.fontSizeCode,
+                terminal: settings.fontSizeTerminal,
+              })}
             />
           </>
         }
