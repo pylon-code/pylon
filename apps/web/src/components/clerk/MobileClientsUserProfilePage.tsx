0bkevin/t3code

feat(connect): deregister account environments from any client (#4844)
Co-authored-by: Julius Marminge <julius0216@outlook.com>
Co-authored-by: codex <codex@users.noreply.github.com>

apps/web/src/components/clerk/MobileClientsUserProfilePage.tsx
@@ -1,8 +1,7 @@
 import type { RelayClientDeviceRecord } from "@t3tools/contracts/relay";
-import { RefreshCwIcon, SmartphoneIcon } from "lucide-react";
+import { SmartphoneIcon } from "lucide-react";
 
 import { useManagedRelayDevices } from "../../cloud/managedRelayState";
-import { cn } from "../../lib/utils";
 import { Badge } from "../ui/badge";
 import { Button } from "../ui/button";
 import { Empty, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from "../ui/empty";
@@ -12,6 +11,11 @@ import {
   mobileClientPlatformLabel,
   mobileClientUpdatedAtLabel,
 } from "./MobileClientsUserProfilePage.logic";
+import {
+  ClerkUserProfilePage,
+  ClerkUserProfileRefreshButton,
+  ClerkUserProfileRow,
+} from "./ClerkUserProfilePage";
 
 const MOBILE_CLIENT_SKELETON_ROWS = ["primary", "secondary"] as const;
 
@@ -31,53 +35,47 @@ function MobileClientStatusBadge({
 
 function MobileClientRow({ device }: { readonly device: RelayClientDeviceRecord }) {
   return (
-    <li className="rounded-xl border bg-card p-4 text-card-foreground shadow-sm/4">
-      <div className="flex items-start gap-3">
-        <div className="flex size-9 shrink-0 items-center justify-center rounded-lg border bg-muted/40 text-muted-foreground">
-          <SmartphoneIcon className="size-4" />
-        </div>
-        <div className="min-w-0 flex-1">
-          <div className="flex flex-col gap-1 sm:flex-row sm:items-start sm:justify-between sm:gap-3">
-            <div className="min-w-0">
-              <h3 className="truncate text-sm font-semibold text-foreground">{device.label}</h3>
-              <p className="text-xs text-muted-foreground">{mobileClientPlatformLabel(device)}</p>
-            </div>
-            <p className="shrink-0 text-[11px] text-muted-foreground/75">
-              {mobileClientUpdatedAtLabel(device.updatedAt)}
-            </p>
-          </div>
-          <div className="mt-3 flex flex-wrap gap-1.5">
-            <MobileClientStatusBadge
-              enabled={device.notifications.enabled}
-              label="Push notifications"
-            />
-            <MobileClientStatusBadge
-              enabled={device.liveActivities.enabled}
-              label="Live Activities"
-            />
-          </div>
-          <p className="mt-2 text-xs leading-relaxed text-muted-foreground/80">
-            {mobileClientNotificationDetail(device)}
+    <ClerkUserProfileRow icon={<SmartphoneIcon className="size-4" />}>
+      <div className="flex flex-col gap-0.5 sm:flex-row sm:items-start sm:justify-between sm:gap-3">
+        <div className="min-w-0">
+          <h3 className="truncate text-[0.8125rem] leading-[1.125rem] font-medium text-foreground">
+            {device.label}
+          </h3>
+          <p className="text-xs leading-[1.125rem] text-muted-foreground">
+            {mobileClientPlatformLabel(device)}
           </p>
         </div>
+        <p className="shrink-0 text-[0.6875rem] leading-4 text-muted-foreground/75">
+          {mobileClientUpdatedAtLabel(device.updatedAt)}
+        </p>
       </div>
-    </li>
+      <div className="mt-2 flex flex-wrap gap-1.5">
+        <MobileClientStatusBadge
+          enabled={device.notifications.enabled}
+          label="Push notifications"
+        />
+        <MobileClientStatusBadge enabled={device.liveActivities.enabled} label="Live Activities" />
+      </div>
+      <p className="mt-1.5 text-xs leading-[1.125rem] text-muted-foreground/80">
+        {mobileClientNotificationDetail(device)}
+      </p>
+    </ClerkUserProfileRow>
   );
 }
 
 function MobileClientsSkeleton() {
   return (
-    <div aria-label="Loading mobile clients" className="space-y-3" role="status">
+    <div aria-label="Loading mobile clients" className="divide-y border-t" role="status">
       {MOBILE_CLIENT_SKELETON_ROWS.map((row) => (
-        <div key={row} className="rounded-xl border p-4">
+        <div key={row} className="py-4">
           <div className="flex gap-3">
-            <Skeleton className="size-9 shrink-0 rounded-lg" />
+            <Skeleton className="size-8 shrink-0 rounded-lg" />
             <div className="flex-1 space-y-2">
-              <Skeleton className="h-4 w-36" />
+              <Skeleton className="h-3.5 w-36" />
               <Skeleton className="h-3 w-28" />
-              <div className="flex gap-2 pt-1">
-                <Skeleton className="h-5 w-28" />
-                <Skeleton className="h-5 w-24" />
+              <div className="flex gap-2">
+                <Skeleton className="h-4.5 w-28" />
+                <Skeleton className="h-4.5 w-24" />
               </div>
             </div>
           </div>
@@ -89,13 +87,13 @@ function MobileClientsSkeleton() {
 
 function EmptyMobileClients() {
   return (
-    <Empty className="min-h-72 rounded-xl border border-dashed bg-muted/15">
-      <EmptyMedia variant="icon">
+    <Empty className="min-h-64 gap-4 border-t px-6 py-10 md:p-10">
+      <EmptyMedia className="mb-0" variant="icon">
         <SmartphoneIcon />
       </EmptyMedia>
       <EmptyHeader>
-        <EmptyTitle>No mobile clients</EmptyTitle>
-        <EmptyDescription>
+        <EmptyTitle className="text-[1.0625rem] leading-6">No mobile clients</EmptyTitle>
+        <EmptyDescription className="text-[0.8125rem] leading-[1.125rem]">
           Sign in to T3 Code on your iPhone to register it for push notifications and Live
           Activities.
         </EmptyDescription>
@@ -112,29 +110,20 @@ export function MobileClientsUserProfilePage() {
   const hasErrorWithoutData = devicesState.error !== null && devicesState.data === null;
 
   return (
-    <div className="flex min-h-[30rem] w-full flex-col bg-background text-foreground">
-      <header className="flex flex-col gap-4 border-b px-6 py-5 sm:flex-row sm:items-center sm:justify-between">
-        <div>
-          <h2 className="text-base font-semibold tracking-[-0.01em]">Mobile clients</h2>
-          <p className="mt-1 text-sm text-muted-foreground">
-            Devices registered to receive T3 Connect activity from your environments.
-          </p>
-        </div>
-        <Button
-          size="sm"
-          variant="outline"
-          disabled={devicesState.isPending}
+    <ClerkUserProfilePage
+      title="Mobile clients"
+      description="Devices registered to receive T3 Connect activity from your environments."
+      action={
+        <ClerkUserProfileRefreshButton
+          isPending={devicesState.isPending}
           onClick={devicesState.refresh}
-        >
-          <RefreshCwIcon className={cn("size-3.5", devicesState.isPending && "animate-spin")} />
-          Refresh
-        </Button>
-      </header>
-
-      <div className="flex-1 p-6">
+        />
+      }
+    >
+      <div>
         {devicesState.error ? (
           <div
-            className="mb-4 flex flex-col gap-3 rounded-lg border border-destructive/25 bg-destructive/5 p-3 text-sm sm:flex-row sm:items-center sm:justify-between"
+            className="mb-4 flex flex-col gap-3 border-t border-destructive/35 py-3 text-[0.8125rem] sm:flex-row sm:items-center sm:justify-between"
             role="alert"
           >
             <div>
@@ -152,7 +141,7 @@ export function MobileClientsUserProfilePage() {
         {isInitialLoad ? (
           <MobileClientsSkeleton />
         ) : hasErrorWithoutData ? null : devices.length > 0 ? (
-          <ul className="space-y-3">
+          <ul className="border-t">
             {devices.map((device) => (
               <MobileClientRow key={device.deviceId} device={device} />
             ))}
@@ -161,6 +150,6 @@ export function MobileClientsUserProfilePage() {
           <EmptyMobileClients />
         )}
       </div>
-    </div>
+    </ClerkUserProfilePage>
   );
 }

apps/web/src/components/clerk/T3ConnectSidebarSignIn.tsx
@@ -1,9 +1,10 @@
 import { UserButton, useAuth } from "@clerk/react";
-import { LogInIcon, SmartphoneIcon } from "lucide-react";
+import { LogInIcon, ServerIcon, SmartphoneIcon } from "lucide-react";
 
 import { hasCloudPublicConfig } from "../../cloud/publicConfig";
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "../ui/sidebar";
 import { MobileClientsUserProfilePage } from "./MobileClientsUserProfilePage";
+import { T3ConnectUserProfilePage } from "./T3ConnectUserProfilePage";
 import { useT3ConnectAuthPrompt } from "./useT3ConnectAuthPrompt";
 
 export function T3ConnectSidebarSignIn() {
@@ -38,6 +39,13 @@ function ConfiguredT3ConnectSidebarAvatar() {
         url="mobile-clients"
       >
         <MobileClientsUserProfilePage />
+      </UserButton.UserProfilePage>
+      <UserButton.UserProfilePage
+        label="T3 Connect"
+        labelIcon={<ServerIcon className="size-4" />}
+        url="t3-connect"
+      >
+        <T3ConnectUserProfilePage />
       </UserButton.UserProfilePage>
     </UserButton>
   );

apps/web/src/components/clerk/T3ConnectUserProfilePage.test.tsx
@@ -1,0 +1,63 @@
+import type { EnvironmentId } from "@t3tools/contracts";
+import type { RelayClientEnvironmentRecord } from "@t3tools/contracts/relay";
+import { renderToStaticMarkup } from "react-dom/server";
+import { describe, expect, it, vi } from "vite-plus/test";
+
+import { T3ConnectEnvironmentRow } from "./T3ConnectUserProfilePage";
+
+const environment: RelayClientEnvironmentRecord = {
+  environmentId: "environment-1" as EnvironmentId,
+  label: "Studio Mac",
+  endpoint: {
+    httpBaseUrl: "https://studio.example.com",
+    wsBaseUrl: "wss://studio.example.com",
+    providerKind: "cloudflare_tunnel",
+  },
+  linkedAt: "2026-08-12T12:00:00.000Z",
+};
+
+function renderRow({
+  confirmationOpen = false,
+  mutationPending = false,
+}: {
+  readonly confirmationOpen?: boolean;
+  readonly mutationPending?: boolean;
+} = {}) {
+  return renderToStaticMarkup(
+    <T3ConnectEnvironmentRow
+      environment={environment}
+      confirmationOpen={confirmationOpen}
+      mutationPending={mutationPending}
+      onConfirmationChange={vi.fn()}
+      onDeregister={vi.fn()}
+    />,
+  );
+}
+
+describe("T3 Connect environment row", () => {
+  it("keeps deregistration confirmation inline and collapsed by default", () => {
+    const markup = renderRow();
+
+    expect(markup).toContain("Studio Mac");
+    expect(markup).toContain("Deregister");
+    expect(markup).not.toContain("Deregister server");
+    expect(markup).not.toContain("Confirm deregistration of Studio Mac");
+  });
+
+  it("expands Clerk-style confirmation content beneath the environment row", () => {
+    const markup = renderRow({ confirmationOpen: true });
+
+    expect(markup).toContain("Deregister server");
+    expect(markup).toContain("“Studio Mac” will be removed from this account.");
+    expect(markup).toContain("Confirm deregistration of Studio Mac");
+    expect(markup).toContain("Local connections on your devices are not changed.");
+    expect(markup).toContain("Cancel");
+  });
+
+  it("locks the confirmation actions while deregistration is pending", () => {
+    const markup = renderRow({ confirmationOpen: true, mutationPending: true });
+
+    expect(markup).toContain("Deregistering…");
+    expect(markup.match(/ disabled=""/g)).toHaveLength(3);
+  });
+});

packages/client-runtime/src/relay/managedRelayState.ts
@@ -2,6 +2,7 @@ import type {
   RelayClientEnvironmentRecord,
   RelayEnvironmentStatusResponse,
 } from "@t3tools/contracts/relay";
+import type { EnvironmentId } from "@t3tools/contracts";
 import {
   RelayEnvironmentConnectScope,
   RelayEnvironmentStatusScope,
@@ -216,6 +217,24 @@ export const waitForManagedRelayClerkToken = Effect.fn(
     readCurrentSession();
     return Effect.sync(() => unsubscribe?.());
   });
+});
+
+/** Removes an environment from the signed-in account without contacting that environment. */
+export const deregisterManagedRelayEnvironment = Effect.fn(
+  "clientRuntime.managedRelaySession.deregisterEnvironment",
+)(function* (
+  registry: AtomRegistry.AtomRegistry,
+  input: { readonly accountId: string; readonly environmentId: EnvironmentId },
+) {
+  const session = registry.get(managedRelaySessionAtom);
+  if (!session || session.accountId !== input.accountId) {
+    return yield* new ManagedRelaySessionError({
+      message: "Sign in to T3 Connect before deregistering an environment.",
+    });
+  }
+  const clerkToken = yield* readSessionClerkToken(session);
+  const relay = yield* ManagedRelay.ManagedRelayClient;
+  yield* relay.unlinkEnvironment({ clerkToken, environmentId: input.environmentId });
 });
 
 function requireClerkToken(

pylon-code/pylon

(cherry picked from commit d0b8d6306b4686528913948b21c8a9eeda01645a)

apps/web/src/components/clerk/MobileClientsUserProfilePage.tsx
@@ -1,8 +1,7 @@
 import type { RelayClientDeviceRecord } from "@t3tools/contracts/relay";
-import { RefreshCwIcon, SmartphoneIcon } from "lucide-react";
+import { SmartphoneIcon } from "lucide-react";
 
 import { useManagedRelayDevices } from "../../cloud/managedRelayState";
-import { cn } from "../../lib/utils";
 import { Badge } from "../ui/badge";
 import { Button } from "../ui/button";
 import { Empty, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from "../ui/empty";
@@ -12,6 +11,11 @@ import {
   mobileClientPlatformLabel,
   mobileClientUpdatedAtLabel,
 } from "./MobileClientsUserProfilePage.logic";
+import {
+  ClerkUserProfilePage,
+  ClerkUserProfileRefreshButton,
+  ClerkUserProfileRow,
+} from "./ClerkUserProfilePage";
 
 const MOBILE_CLIENT_SKELETON_ROWS = ["primary", "secondary"] as const;
 
@@ -31,53 +35,47 @@ function MobileClientStatusBadge({
 
 function MobileClientRow({ device }: { readonly device: RelayClientDeviceRecord }) {
   return (
-    <li className="rounded-xl border bg-card p-4 text-card-foreground shadow-sm/4">
-      <div className="flex items-start gap-3">
-        <div className="flex size-9 shrink-0 items-center justify-center rounded-lg border bg-muted/40 text-muted-foreground">
-          <SmartphoneIcon className="size-4" />
-        </div>
-        <div className="min-w-0 flex-1">
-          <div className="flex flex-col gap-1 sm:flex-row sm:items-start sm:justify-between sm:gap-3">
-            <div className="min-w-0">
-              <h3 className="truncate text-sm font-semibold text-foreground">{device.label}</h3>
-              <p className="text-xs text-muted-foreground">{mobileClientPlatformLabel(device)}</p>
-            </div>
-            <p className="shrink-0 text-[11px] text-muted-foreground/75">
-              {mobileClientUpdatedAtLabel(device.updatedAt)}
-            </p>
-          </div>
-          <div className="mt-3 flex flex-wrap gap-1.5">
-            <MobileClientStatusBadge
-              enabled={device.notifications.enabled}
-              label="Push notifications"
-            />
-            <MobileClientStatusBadge
-              enabled={device.liveActivities.enabled}
-              label="Live Activities"
-            />
-          </div>
-          <p className="mt-2 text-xs leading-relaxed text-muted-foreground/80">
-            {mobileClientNotificationDetail(device)}
+    <ClerkUserProfileRow icon={<SmartphoneIcon className="size-4" />}>
+      <div className="flex flex-col gap-0.5 sm:flex-row sm:items-start sm:justify-between sm:gap-3">
+        <div className="min-w-0">
+          <h3 className="truncate text-[0.8125rem] leading-[1.125rem] font-medium text-foreground">
+            {device.label}
+          </h3>
+          <p className="text-xs leading-[1.125rem] text-muted-foreground">
+            {mobileClientPlatformLabel(device)}
           </p>
         </div>
+        <p className="shrink-0 text-[0.6875rem] leading-4 text-muted-foreground/75">
+          {mobileClientUpdatedAtLabel(device.updatedAt)}
+        </p>
       </div>
-    </li>
+      <div className="mt-2 flex flex-wrap gap-1.5">
+        <MobileClientStatusBadge
+          enabled={device.notifications.enabled}
+          label="Push notifications"
+        />
+        <MobileClientStatusBadge enabled={device.liveActivities.enabled} label="Live Activities" />
+      </div>
+      <p className="mt-1.5 text-xs leading-[1.125rem] text-muted-foreground/80">
+        {mobileClientNotificationDetail(device)}
+      </p>
+    </ClerkUserProfileRow>
   );
 }
 
 function MobileClientsSkeleton() {
   return (
-    <div aria-label="Loading mobile clients" className="space-y-3" role="status">
+    <div aria-label="Loading mobile clients" className="divide-y border-t" role="status">
       {MOBILE_CLIENT_SKELETON_ROWS.map((row) => (
-        <div key={row} className="rounded-xl border p-4">
+        <div key={row} className="py-4">
           <div className="flex gap-3">
-            <Skeleton className="size-9 shrink-0 rounded-lg" />
+            <Skeleton className="size-8 shrink-0 rounded-lg" />
             <div className="flex-1 space-y-2">
-              <Skeleton className="h-4 w-36" />
+              <Skeleton className="h-3.5 w-36" />
               <Skeleton className="h-3 w-28" />
-              <div className="flex gap-2 pt-1">
-                <Skeleton className="h-5 w-28" />
-                <Skeleton className="h-5 w-24" />
+              <div className="flex gap-2">
+                <Skeleton className="h-4.5 w-28" />
+                <Skeleton className="h-4.5 w-24" />
               </div>
             </div>
           </div>
@@ -89,13 +87,13 @@ function MobileClientsSkeleton() {
 
 function EmptyMobileClients() {
   return (
-    <Empty className="min-h-72 rounded-xl border border-dashed bg-muted/15">
-      <EmptyMedia variant="icon">
+    <Empty className="min-h-64 gap-4 border-t px-6 py-10 md:p-10">
+      <EmptyMedia className="mb-0" variant="icon">
         <SmartphoneIcon />
       </EmptyMedia>
       <EmptyHeader>
-        <EmptyTitle>No mobile clients</EmptyTitle>
-        <EmptyDescription>
+        <EmptyTitle className="text-[1.0625rem] leading-6">No mobile clients</EmptyTitle>
+        <EmptyDescription className="text-[0.8125rem] leading-[1.125rem]">
           Sign in to Pylon on your iPhone to register it for push notifications and Live Activities.
         </EmptyDescription>
       </EmptyHeader>
@@ -111,29 +109,20 @@ export function MobileClientsUserProfilePage() {
   const hasErrorWithoutData = devicesState.error !== null && devicesState.data === null;
 
   return (
-    <div className="flex min-h-[30rem] w-full flex-col bg-background text-foreground">
-      <header className="flex flex-col gap-4 border-b px-6 py-5 sm:flex-row sm:items-center sm:justify-between">
-        <div>
-          <h2 className="text-base font-semibold tracking-[-0.01em]">Mobile clients</h2>
-          <p className="mt-1 text-sm text-muted-foreground">
-            Devices registered to receive Pylon Connect activity from your environments.
-          </p>
-        </div>
-        <Button
-          size="sm"
-          variant="outline"
-          disabled={devicesState.isPending}
+    <ClerkUserProfilePage
+      title="Mobile clients"
+      description="Devices registered to receive Pylon Connect activity from your environments."
+      action={
+        <ClerkUserProfileRefreshButton
+          isPending={devicesState.isPending}
           onClick={devicesState.refresh}
-        >
-          <RefreshCwIcon className={cn("size-3.5", devicesState.isPending && "animate-spin")} />
-          Refresh
-        </Button>
-      </header>
-
-      <div className="flex-1 p-6">
+        />
+      }
+    >
+      <div>
         {devicesState.error ? (
           <div
-            className="mb-4 flex flex-col gap-3 rounded-lg border border-destructive/25 bg-destructive/5 p-3 text-sm sm:flex-row sm:items-center sm:justify-between"
+            className="mb-4 flex flex-col gap-3 border-t border-destructive/35 py-3 text-[0.8125rem] sm:flex-row sm:items-center sm:justify-between"
             role="alert"
           >
             <div>
@@ -151,7 +140,7 @@ export function MobileClientsUserProfilePage() {
         {isInitialLoad ? (
           <MobileClientsSkeleton />
         ) : hasErrorWithoutData ? null : devices.length > 0 ? (
-          <ul className="space-y-3">
+          <ul className="border-t">
             {devices.map((device) => (
               <MobileClientRow key={device.deviceId} device={device} />
             ))}
@@ -160,6 +149,6 @@ export function MobileClientsUserProfilePage() {
           <EmptyMobileClients />
         )}
       </div>
-    </div>
+    </ClerkUserProfilePage>
   );
 }

apps/web/src/components/clerk/T3ConnectSidebarSignIn.tsx
@@ -1,9 +1,10 @@
 import { UserButton, useAuth } from "@clerk/react";
-import { LogInIcon, SmartphoneIcon } from "lucide-react";
+import { LogInIcon, ServerIcon, SmartphoneIcon } from "lucide-react";
 
 import { hasCloudPublicConfig } from "../../cloud/publicConfig";
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "../ui/sidebar";
 import { MobileClientsUserProfilePage } from "./MobileClientsUserProfilePage";
+import { T3ConnectUserProfilePage } from "./T3ConnectUserProfilePage";
 import { useT3ConnectAuthPrompt } from "./useT3ConnectAuthPrompt";
 
 export function T3ConnectSidebarSignIn() {
@@ -38,6 +39,13 @@ function ConfiguredT3ConnectSidebarAvatar() {
         url="mobile-clients"
       >
         <MobileClientsUserProfilePage />
+      </UserButton.UserProfilePage>
+      <UserButton.UserProfilePage
+        label="Pylon Connect"
+        labelIcon={<ServerIcon className="size-4" />}
+        url="t3-connect"
+      >
+        <T3ConnectUserProfilePage />
       </UserButton.UserProfilePage>
     </UserButton>
   );

apps/web/src/components/clerk/T3ConnectUserProfilePage.test.tsx
@@ -1,0 +1,63 @@
+import type { EnvironmentId } from "@t3tools/contracts";
+import type { RelayClientEnvironmentRecord } from "@t3tools/contracts/relay";
+import { renderToStaticMarkup } from "react-dom/server";
+import { describe, expect, it, vi } from "vite-plus/test";
+
+import { T3ConnectEnvironmentRow } from "./T3ConnectUserProfilePage";
+
+const environment: RelayClientEnvironmentRecord = {
+  environmentId: "environment-1" as EnvironmentId,
+  label: "Studio Mac",
+  endpoint: {
+    httpBaseUrl: "https://studio.example.com",
+    wsBaseUrl: "wss://studio.example.com",
+    providerKind: "cloudflare_tunnel",
+  },
+  linkedAt: "2026-08-12T12:00:00.000Z",
+};
+
+function renderRow({
+  confirmationOpen = false,
+  mutationPending = false,
+}: {
+  readonly confirmationOpen?: boolean;
+  readonly mutationPending?: boolean;
+} = {}) {
+  return renderToStaticMarkup(
+    <T3ConnectEnvironmentRow
+      environment={environment}
+      confirmationOpen={confirmationOpen}
+      mutationPending={mutationPending}
+      onConfirmationChange={vi.fn()}
+      onDeregister={vi.fn()}
+    />,
+  );
+}
+
+describe("Pylon Connect environment row", () => {
+  it("keeps deregistration confirmation inline and collapsed by default", () => {
+    const markup = renderRow();
+
+    expect(markup).toContain("Studio Mac");
+    expect(markup).toContain("Deregister");
+    expect(markup).not.toContain("Deregister server");
+    expect(markup).not.toContain("Confirm deregistration of Studio Mac");
+  });
+
+  it("expands Clerk-style confirmation content beneath the environment row", () => {
+    const markup = renderRow({ confirmationOpen: true });
+
+    expect(markup).toContain("Deregister server");
+    expect(markup).toContain("“Studio Mac” will be removed from this account.");
+    expect(markup).toContain("Confirm deregistration of Studio Mac");
+    expect(markup).toContain("Local connections on your devices are not changed.");
+    expect(markup).toContain("Cancel");
+  });
+
+  it("locks the confirmation actions while deregistration is pending", () => {
+    const markup = renderRow({ confirmationOpen: true, mutationPending: true });
+
+    expect(markup).toContain("Deregistering…");
+    expect(markup.match(/ disabled=""/g)).toHaveLength(3);
+  });
+});

packages/client-runtime/src/relay/managedRelayState.ts
@@ -2,6 +2,7 @@ import type {
   RelayClientEnvironmentRecord,
   RelayEnvironmentStatusResponse,
 } from "@t3tools/contracts/relay";
+import type { EnvironmentId } from "@t3tools/contracts";
 import {
   RelayEnvironmentConnectScope,
   RelayEnvironmentStatusScope,
@@ -216,6 +217,24 @@ export const waitForManagedRelayClerkToken = Effect.fn(
     readCurrentSession();
     return Effect.sync(() => unsubscribe?.());
   });
+});
+
+/** Removes an environment from the signed-in account without contacting that environment. */
+export const deregisterManagedRelayEnvironment = Effect.fn(
+  "clientRuntime.managedRelaySession.deregisterEnvironment",
+)(function* (
+  registry: AtomRegistry.AtomRegistry,
+  input: { readonly accountId: string; readonly environmentId: EnvironmentId },
+) {
+  const session = registry.get(managedRelaySessionAtom);
+  if (!session || session.accountId !== input.accountId) {
+    return yield* new ManagedRelaySessionError({
+      message: "Sign in to Pylon Connect before deregistering an environment.",
+    });
+  }
+  const clerkToken = yield* readSessionClerkToken(session);
+  const relay = yield* ManagedRelay.ManagedRelayClient;
+  yield* relay.unlinkEnvironment({ clerkToken, environmentId: input.environmentId });
 });
 
 function requireClerkToken(