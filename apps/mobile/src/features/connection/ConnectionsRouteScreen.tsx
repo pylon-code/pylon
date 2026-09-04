e115116380d2d6c67db5eff8310a8fde996e87d8
Julius Marminge <julius0216@outlook.com>
2026-09-04 09:52:01 -0700

    fix(mobile): remove provider setup (#9721)
    (cherry picked from commit 9eb4d71681dc7d002082db2f8b4bacf7614412f4)

--- apps/mobile/src/features/connection/ConnectionsRouteScreen.tsx
+++ apps/mobile/src/features/connection/ConnectionsRouteScreen.tsx
@@ -75,12 +75,6 @@ export function ConnectionsRouteScreen() {
                   onReconnect={onReconnectEnvironment}
                   onRemove={onRemoveEnvironmentPress}
                   onUpdate={onUpdateEnvironment}
-                  onSetupProvider={(target) =>
-                    navigation.navigate("SettingsProviderSetup", {
-                      environmentId: target.environmentId,
-                      instanceId: target.instanceId,
-                    })
-                  }
                 />
               </View>
             ))}
--- apps/mobile/src/features/settings/SettingsEnvironmentsRouteScreen.tsx
+++ apps/mobile/src/features/settings/SettingsEnvironmentsRouteScreen.tsx
@@ -137,12 +137,6 @@ export function SettingsEnvironmentsRouteScreen() {
                   onReconnect={onReconnectEnvironment}
                   onRemove={onRemoveEnvironmentPress}
                   onUpdate={handleUpdateEnvironment}
-                  onSetupProvider={(target) =>
-                    navigation.navigate("SettingsProviderSetup", {
-                      environmentId: target.environmentId,
-                      instanceId: target.instanceId,
-                    })
-                  }
                 />
               </View>
             ))}
