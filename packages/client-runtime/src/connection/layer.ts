cc2eddc3294f933cffcf8671d7eb62d31db4cb03
Julius Marminge <julius0216@outlook.com>
2026-09-04 20:12:04 -0700

    fix(connect): refresh HTTP credentials without reconnecting (#9594)
    (cherry picked from commit 363cde4114c2186c496ed97cafba4a8737ea5585)

--- packages/client-runtime/src/connection/layer.ts
+++ packages/client-runtime/src/connection/layer.ts
@@ -11,12 +11,8 @@ import * as RelayEnvironmentDiscovery from "../relay/discovery.ts";
 import * as RemoteEnvironmentAuthorization from "../authorization/service.ts";
 import * as RpcSession from "../rpc/session.ts";
 
-const resolverLayer = ConnectionResolver.layer.pipe(
-  Layer.provide(RemoteEnvironmentAuthorization.layer),
-);
-
 const driverLayer = ConnectionDriver.layer.pipe(
-  Layer.provide(Layer.mergeAll(resolverLayer, RpcSession.layer)),
+  Layer.provide(Layer.mergeAll(ConnectionResolver.layer, RpcSession.layer)),
 );
 
 const registryLayer = EnvironmentRegistry.layer.pipe(Layer.provide(driverLayer));
@@ -41,4 +37,7 @@ const connectionStartupLayer = Layer.effectDiscard(
   }).pipe(Effect.withSpan("clientRuntime.connection.application.start")),
 );
 
-export const layer = connectionStartupLayer.pipe(Layer.provideMerge(connectionServicesLayer));
+export const layer = connectionStartupLayer.pipe(
+  Layer.provideMerge(connectionServicesLayer),
+  Layer.provideMerge(RemoteEnvironmentAuthorization.layer),
+);
