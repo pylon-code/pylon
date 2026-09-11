nico2525nn/t3code

feat(mobile): add Android agent notifications and ongoing activity (#10416)
Co-authored-by: Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>

infra/relay/scripts/android-push-smoke.ts
@@ -1,0 +1,152 @@
+// @effect-diagnostics nodeBuiltinImport:off - This developer command reads local credential files.
+import * as NodeFSP from "node:fs/promises";
+import * as NodeRuntime from "@effect/platform-node/NodeRuntime";
+import * as Effect from "effect/Effect";
+import * as Clock from "effect/Clock";
+import * as Layer from "effect/Layer";
+import * as Redacted from "effect/Redacted";
+import * as Schema from "effect/Schema";
+import * as FetchHttpClient from "effect/unstable/http/FetchHttpClient";
+
+import * as RelayConfiguration from "../src/Config.ts";
+import * as WebCrypto from "../src/WebCrypto.ts";
+import * as FcmAssertionSigner from "../src/agentActivity/FcmAssertionSigner.ts";
+import * as FcmClient from "../src/agentActivity/FcmClient.ts";
+
+const Device = Schema.Struct({
+  token: Schema.NonEmptyString,
+  deviceId: Schema.NonEmptyString,
+  userId: Schema.NonEmptyString,
+  packageName: Schema.NonEmptyString,
+  deepLink: Schema.optional(Schema.NonEmptyString),
+});
+const decodeDevice = Schema.decodeUnknownEffect(Schema.fromJsonString(Device));
+const Phase = Schema.Literals(["running", "approval", "input", "completed", "failed", "end"]);
+const decodePhase = Schema.decodeUnknownEffect(Phase);
+
+class SmokeUsageError extends Schema.TaggedError<SmokeUsageError>()("SmokeUsageError", {}) {
+  override get message() {
+    return "Usage: node scripts/android-push-smoke.ts <service-account.json> <device.json> <running|approval|input|completed|failed|end>";
+  }
+}
+
+class SmokeCredentialReadError extends Schema.TaggedError<SmokeCredentialReadError>()(
+  "SmokeCredentialReadError",
+  { cause: Schema.Defect() },
+) {
+  override get message() {
+    return "Could not read service-account file.";
+  }
+}
+
+class SmokeDeviceReadError extends Schema.TaggedError<SmokeDeviceReadError>()(
+  "SmokeDeviceReadError",
+  { cause: Schema.Defect() },
+) {
+  override get message() {
+    return "Could not read device file.";
+  }
+}
+
+class SmokeUnregisteredDeviceError extends Schema.TaggedError<SmokeUnregisteredDeviceError>()(
+  "SmokeUnregisteredDeviceError",
+  {},
+) {
+  override get message() {
+    return "This device token is no longer registered with Firebase.";
+  }
+}
+
+const main = Effect.gen(function* () {
+  const [credentialPath, devicePath, phaseArg] = process.argv.slice(2);
+  if (!credentialPath || !devicePath || !phaseArg) return yield* new SmokeUsageError({});
+  const phase = yield* decodePhase(phaseArg);
+  const credentials = yield* Effect.tryPromise({
+    try: () => NodeFSP.readFile(credentialPath, "utf8"),
+    catch: (cause) => new SmokeCredentialReadError({ cause }),
+  });
+  const device = yield* Effect.tryPromise({
+    try: () => NodeFSP.readFile(devicePath, "utf8"),
+    catch: (cause) => new SmokeDeviceReadError({ cause }),
+  }).pipe(Effect.flatMap(decodeDevice));
+  const title =
+    phase === "completed"
+      ? "Agent finished"
+      : phase === "failed"
+        ? "Agent failed"
+        : phase === "approval"
+          ? "Approval needed"
+          : phase === "input"
+            ? "Input needed"
+            : null;
+  const active = phase === "running" || phase === "approval" || phase === "input";
+  const now = yield* Clock.currentTimeMillis;
+  const config: RelayConfiguration.RelayConfiguration["Service"] = {
+    relayIssuer: "http://localhost",
+    fcmServiceAccount: Redacted.make(credentials),
+    apns: null,
+    clerkSecretKey: Redacted.make(""),
+    clerkPublishableKey: "",
+    clerkJwtAudience: "",
+    apnsDeliveryJobSigningSecret: Redacted.make(""),
+    cloudMintPrivateKey: Redacted.make(""),
+    cloudMintPublicKey: "",
+    managedEndpointBaseDomain: undefined,
+    managedEndpointNamespace: undefined,
+  };
+  const result = yield* FcmClient.FcmClient.pipe(
+    Effect.flatMap((client) =>
+      client.send({
+        token: device.token,
+        packageName: device.packageName,
+        alert: title !== null,
+        data: {
+          t3_kind: "agent_activity",
+          device_id: device.deviceId,
+          user_id: device.userId,
+          updated_at: String(now),
+          active: String(active),
+          activity_title: active
+            ? "1 active agent"
+            : phase === "failed"
+              ? "Agent work failed"
+              : "Agent work completed",
+          activity_expires_at: String(
+            phase === "end" ? 0 : now + (active ? 2 * 60 * 60 * 1000 : 15 * 60 * 1000),
+          ),
+          activity_body: "Android notification test",
+          activity_path: device.deepLink ?? "/",
+          ...(title
+            ? {
+                alert_id: `smoke-${now}`,
+                alert_title: title,
+                alert_body: "T3 Code Android push test",
+                alert_path: device.deepLink ?? "/",
+              }
+            : {}),
+        },
+      }),
+    ),
+    Effect.provide(
+      FcmClient.layer.pipe(
+        Layer.provide(
+          FcmAssertionSigner.layer.pipe(
+            Layer.provide(Layer.succeed(WebCrypto.WebCrypto, { subtle: globalThis.crypto.subtle })),
+          ),
+        ),
+        Layer.provide(
+          Layer.mergeAll(
+            Layer.succeed(RelayConfiguration.RelayConfiguration, config),
+            FetchHttpClient.layer,
+          ),
+        ),
+      ),
+    ),
+  );
+  if (result.unregistered) return yield* new SmokeUnregisteredDeviceError({});
+  yield* Effect.logInfo(
+    `Firebase accepted the ${phase} notification. Verify delivery on the device.`,
+  );
+});
+
+NodeRuntime.runMain(main);

infra/relay/src/agentActivity/FcmAssertionSigner.ts
@@ -1,0 +1,76 @@
+import * as Context from "effect/Context";
+import * as Effect from "effect/Effect";
+import * as Layer from "effect/Layer";
+import * as Schema from "effect/Schema";
+
+import * as WebCrypto from "../WebCrypto.ts";
+
+const encodeJson = Schema.encodeSync(Schema.fromJsonString(Schema.Unknown));
+
+export class FcmAssertionSigningError extends Schema.TaggedError<FcmAssertionSigningError>()(
+  "FcmAssertionSigningError",
+  { cause: Schema.Defect() },
+) {
+  override get message() {
+    return "Failed to sign Firebase authorization assertion.";
+  }
+}
+
+function base64Url(bytes: Uint8Array): string {
+  return btoa(String.fromCharCode(...bytes))
+    .replaceAll("+", "-")
+    .replaceAll("/", "_")
+    .replace(/=+$/, "");
+}
+
+export class FcmAssertionSigner extends Context.Service<
+  FcmAssertionSigner,
+  {
+    readonly sign: (input: {
+      readonly privateKey: string;
+      readonly clientEmail: string;
+      readonly issuedAt: number;
+    }) => Effect.Effect<string, FcmAssertionSigningError>;
+  }
+>()("t3code-relay/agentActivity/FcmAssertionSigner") {}
+
+export const make = Effect.gen(function* () {
+  const { subtle } = yield* WebCrypto.WebCrypto;
+  return FcmAssertionSigner.of({
+    sign: Effect.fn("relay.fcm.assertion")(function* (input) {
+      return yield* Effect.tryPromise({
+        try: async () => {
+          const encoder = new TextEncoder();
+          const header = base64Url(encoder.encode(encodeJson({ alg: "RS256", typ: "JWT" })));
+          const claims = base64Url(
+            encoder.encode(
+              encodeJson({
+                iss: input.clientEmail,
+                scope: "https://www.googleapis.com/auth/firebase.messaging",
+                aud: "https://oauth2.googleapis.com/token",
+                iat: input.issuedAt,
+                exp: input.issuedAt + 3600,
+              }),
+            ),
+          );
+          const pem = input.privateKey.replace(/-----[^-]+-----|\s/g, "");
+          const key = await subtle.importKey(
+            "pkcs8",
+            Uint8Array.from(atob(pem), (c) => c.charCodeAt(0)),
+            { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" },
+            false,
+            ["sign"],
+          );
+          const signature = await subtle.sign(
+            "RSASSA-PKCS1-v1_5",
+            key,
+            encoder.encode(`${header}.${claims}`),
+          );
+          return `${header}.${claims}.${base64Url(new Uint8Array(signature))}`;
+        },
+        catch: (cause) => new FcmAssertionSigningError({ cause }),
+      });
+    }),
+  });
+});
+export const layer = Layer.effect(FcmAssertionSigner, make);

pylon-code/pylon

(cherry picked from commit 6c583620ff7ad3235b135af7107c0543467eecfa)

infra/relay/scripts/android-push-smoke.ts
@@ -1,0 +1,152 @@
+// @effect-diagnostics nodeBuiltinImport:off - This developer command reads local credential files.
+import * as NodeFSP from "node:fs/promises";
+import * as NodeRuntime from "@effect/platform-node/NodeRuntime";
+import * as Effect from "effect/Effect";
+import * as Clock from "effect/Clock";
+import * as Layer from "effect/Layer";
+import * as Redacted from "effect/Redacted";
+import * as Schema from "effect/Schema";
+import * as FetchHttpClient from "effect/unstable/http/FetchHttpClient";
+
+import * as RelayConfiguration from "../src/Config.ts";
+import * as WebCrypto from "../src/WebCrypto.ts";
+import * as FcmAssertionSigner from "../src/agentActivity/FcmAssertionSigner.ts";
+import * as FcmClient from "../src/agentActivity/FcmClient.ts";
+
+const Device = Schema.Struct({
+  token: Schema.NonEmptyString,
+  deviceId: Schema.NonEmptyString,
+  userId: Schema.NonEmptyString,
+  packageName: Schema.NonEmptyString,
+  deepLink: Schema.optional(Schema.NonEmptyString),
+});
+const decodeDevice = Schema.decodeUnknownEffect(Schema.fromJsonString(Device));
+const Phase = Schema.Literals(["running", "approval", "input", "completed", "failed", "end"]);
+const decodePhase = Schema.decodeUnknownEffect(Phase);
+
+class SmokeUsageError extends Schema.TaggedErrorClass<SmokeUsageError>()("SmokeUsageError", {}) {
+  override get message() {
+    return "Usage: node scripts/android-push-smoke.ts <service-account.json> <device.json> <running|approval|input|completed|failed|end>";
+  }
+}
+
+class SmokeCredentialReadError extends Schema.TaggedErrorClass<SmokeCredentialReadError>()(
+  "SmokeCredentialReadError",
+  { cause: Schema.Defect() },
+) {
+  override get message() {
+    return "Could not read service-account file.";
+  }
+}
+
+class SmokeDeviceReadError extends Schema.TaggedErrorClass<SmokeDeviceReadError>()(
+  "SmokeDeviceReadError",
+  { cause: Schema.Defect() },
+) {
+  override get message() {
+    return "Could not read device file.";
+  }
+}
+
+class SmokeUnregisteredDeviceError extends Schema.TaggedErrorClass<SmokeUnregisteredDeviceError>()(
+  "SmokeUnregisteredDeviceError",
+  {},
+) {
+  override get message() {
+    return "This device token is no longer registered with Firebase.";
+  }
+}
+
+const main = Effect.gen(function* () {
+  const [credentialPath, devicePath, phaseArg] = process.argv.slice(2);
+  if (!credentialPath || !devicePath || !phaseArg) return yield* new SmokeUsageError({});
+  const phase = yield* decodePhase(phaseArg);
+  const credentials = yield* Effect.tryPromise({
+    try: () => NodeFSP.readFile(credentialPath, "utf8"),
+    catch: (cause) => new SmokeCredentialReadError({ cause }),
+  });
+  const device = yield* Effect.tryPromise({
+    try: () => NodeFSP.readFile(devicePath, "utf8"),
+    catch: (cause) => new SmokeDeviceReadError({ cause }),
+  }).pipe(Effect.flatMap(decodeDevice));
+  const title =
+    phase === "completed"
+      ? "Agent finished"
+      : phase === "failed"
+        ? "Agent failed"
+        : phase === "approval"
+          ? "Approval needed"
+          : phase === "input"
+            ? "Input needed"
+            : null;
+  const active = phase === "running" || phase === "approval" || phase === "input";
+  const now = yield* Clock.currentTimeMillis;
+  const config: RelayConfiguration.RelayConfiguration["Service"] = {
+    relayIssuer: "http://localhost",
+    fcmServiceAccount: Redacted.make(credentials),
+    apns: null,
+    clerkSecretKey: Redacted.make(""),
+    clerkPublishableKey: "",
+    clerkJwtAudience: "",
+    apnsDeliveryJobSigningSecret: Redacted.make(""),
+    cloudMintPrivateKey: Redacted.make(""),
+    cloudMintPublicKey: "",
+    managedEndpointBaseDomain: undefined,
+    managedEndpointNamespace: undefined,
+  };
+  const result = yield* FcmClient.FcmClient.pipe(
+    Effect.flatMap((client) =>
+      client.send({
+        token: device.token,
+        packageName: device.packageName,
+        alert: title !== null,
+        data: {
+          t3_kind: "agent_activity",
+          device_id: device.deviceId,
+          user_id: device.userId,
+          updated_at: String(now),
+          active: String(active),
+          activity_title: active
+            ? "1 active agent"
+            : phase === "failed"
+              ? "Agent work failed"
+              : "Agent work completed",
+          activity_expires_at: String(
+            phase === "end" ? 0 : now + (active ? 2 * 60 * 60 * 1000 : 15 * 60 * 1000),
+          ),
+          activity_body: "Android notification test",
+          activity_path: device.deepLink ?? "/",
+          ...(title
+            ? {
+                alert_id: `smoke-${now}`,
+                alert_title: title,
+                alert_body: "Pylon Android push test",
+                alert_path: device.deepLink ?? "/",
+              }
+            : {}),
+        },
+      }),
+    ),
+    Effect.provide(
+      FcmClient.layer.pipe(
+        Layer.provide(
+          FcmAssertionSigner.layer.pipe(
+            Layer.provide(Layer.succeed(WebCrypto.WebCrypto, { subtle: globalThis.crypto.subtle })),
+          ),
+        ),
+        Layer.provide(
+          Layer.mergeAll(
+            Layer.succeed(RelayConfiguration.RelayConfiguration, config),
+            FetchHttpClient.layer,
+          ),
+        ),
+      ),
+    ),
+  );
+  if (result.unregistered) return yield* new SmokeUnregisteredDeviceError({});
+  yield* Effect.logInfo(
+    `Firebase accepted the ${phase} notification. Verify delivery on the device.`,
+  );
+});
+
+NodeRuntime.runMain(main);

infra/relay/src/agentActivity/FcmAssertionSigner.ts
@@ -1,0 +1,76 @@
+import * as Context from "effect/Context";
+import * as Effect from "effect/Effect";
+import * as Layer from "effect/Layer";
+import * as Schema from "effect/Schema";
+
+import * as WebCrypto from "../WebCrypto.ts";
+
+const encodeJson = Schema.encodeSync(Schema.fromJsonString(Schema.Unknown));
+
+export class FcmAssertionSigningError extends Schema.TaggedErrorClass<FcmAssertionSigningError>()(
+  "FcmAssertionSigningError",
+  { cause: Schema.Defect() },
+) {
+  override get message() {
+    return "Failed to sign Firebase authorization assertion.";
+  }
+}
+
+function base64Url(bytes: Uint8Array): string {
+  return btoa(String.fromCharCode(...bytes))
+    .replaceAll("+", "-")
+    .replaceAll("/", "_")
+    .replace(/=+$/, "");
+}
+
+export class FcmAssertionSigner extends Context.Service<
+  FcmAssertionSigner,
+  {
+    readonly sign: (input: {
+      readonly privateKey: string;
+      readonly clientEmail: string;
+      readonly issuedAt: number;
+    }) => Effect.Effect<string, FcmAssertionSigningError>;
+  }
+>()("t3code-relay/agentActivity/FcmAssertionSigner") {}
+
+export const make = Effect.gen(function* () {
+  const { subtle } = yield* WebCrypto.WebCrypto;
+  return FcmAssertionSigner.of({
+    sign: Effect.fn("relay.fcm.assertion")(function* (input) {
+      return yield* Effect.tryPromise({
+        try: async () => {
+          const encoder = new TextEncoder();
+          const header = base64Url(encoder.encode(encodeJson({ alg: "RS256", typ: "JWT" })));
+          const claims = base64Url(
+            encoder.encode(
+              encodeJson({
+                iss: input.clientEmail,
+                scope: "https://www.googleapis.com/auth/firebase.messaging",
+                aud: "https://oauth2.googleapis.com/token",
+                iat: input.issuedAt,
+                exp: input.issuedAt + 3600,
+              }),
+            ),
+          );
+          const pem = input.privateKey.replace(/-----[^-]+-----|\s/g, "");
+          const key = await subtle.importKey(
+            "pkcs8",
+            Uint8Array.from(atob(pem), (c) => c.charCodeAt(0)),
+            { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" },
+            false,
+            ["sign"],
+          );
+          const signature = await subtle.sign(
+            "RSASSA-PKCS1-v1_5",
+            key,
+            encoder.encode(`${header}.${claims}`),
+          );
+          return `${header}.${claims}.${base64Url(new Uint8Array(signature))}`;
+        },
+        catch: (cause) => new FcmAssertionSigningError({ cause }),
+      });
+    }),
+  });
+});
+export const layer = Layer.effect(FcmAssertionSigner, make);